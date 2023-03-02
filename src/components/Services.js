dab8c738ec2af4d32773e386a0be63c610668a03
Moubarakdev <Moubarakkerim@gmail.com>
2023-03-02 16:16:36 +0000

    first commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,96 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+// icon
+import { BsArrowUpRight } from "react-icons/bs";
+// motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
+
+const services = [
+  {
+    name: "Web Development",
+    description:
+      "Donec rutrum congue leo eget malesuada. nibh. lolflrb hehehe hejhekekee ehehe  ",
+    link: "Learn more",
+  },
+  {
+    name: "Web Development",
+    description:
+      "Donec rutrum congue leo eget malesuada. nibh. lolflrb hehehe hejhekekee ehehe  ",
+    link: "Learn more",
+  },
+  {
+    name: "Web Development",
+    description:
+      "Donec rutrum congue leo eget malesuada. nibh. lolflrb hehehe hejhekekee ehehe  ",
+    link: "Learn more",
+  }
+];
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="section " id="services">
+      <div className="container mx-auto ">
+        <div className="flex flex-col lg:flex-row">
+          {/* Text & Image*/}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
+          >
+            <h2 className="h2 text-accent mb-6"> What I Do</h2>
+            <h3 className="h3 max-w-{455px} mb-16">
+              I'm a Freelance Developer with over 1 year of experience.
+            </h3>
+            <button className="btn btn-sm">See my work</button>
+          </motion.div>
+          {/* services */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            {/* Services lists */}
+            <div>
+              {services.map((service, index) => {
+                return (
+                  <div
+                    className=" border-b border-white/80 h-[146] mb-[38px] flex"
+                    key={index}
+                  >
+                    <div className="max-w-[470px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {service.name}
+                      </h4>
+                      <p className="font-secondary leading-tight ">
+                        {service.description}
+                      </p>
+                      <div className=" flex flex-1 flex-col items-end">
+                        <a
+                          href="#"
+                          className=" flex btn w-9 h-9 mb-[42px] justify-center items-center"
+                        >
+                          <BsArrowUpRight />
+                        </a>
+                        <a href="#" className="text-gradient text-sm ">
+                          {service.link}
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,108 @@
-import React from 'react';
+import React from "react";
+// motion
+import { motion } from "framer-motion";
+// vzariant
+import { fadeIn } from "../variants";
+// image
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <section className="section " id="work">
+      <div className=" container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0 "
+          >
+            {/* Text */}
+            <div>
+              <h2 className="h2 leading-light text-accent">
+                My Latest <br />
+                work.
+              </h2>
+              <p className="max-w-sm mb-16">
+                Patratis principis quisquam principis proscripti paene absolutum
+                facile et is actique actique sunt cruore catenis parabat adventu
+                
+              </p>
+              <button className="btn btn-sm">View all projects</button>
+            </div>
+            {/* Image1 */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* Overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* Img */}
+              <img
+                className=" group-hover:scale-125 transition-all duration-500 "
+                src={Img1}
+                alt=""
+              />
+              {/* Pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* Title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50">
+                <span className="text 3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-10"
+          >
+            {/* Image2 */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* Overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* Img */}
+              <img
+                className=" group-hover:scale-125 transition-all duration-500 "
+                src={Img2}
+                alt=""
+              />
+              {/* Pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* Title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50">
+                <span className="text 3xl text-white">Project Title</span>
+              </div>
+            </div>
+            {/* Image3 */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* Overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* Img */}
+              <img
+                className=" group-hover:scale-125 transition-all duration-500 "
+                src={Img3}
+                alt=""
+              />
+              {/* Pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* Title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50">
+                <span className="text 3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
